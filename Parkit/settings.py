9496f52b5b718f2e97d88eb4aa54483152a74b4d
halima precious <halimaprecious3@gmail.com>
2022-06-22 09:42:32 +0300

    updated urls

--- Parkit/settings.py
+++ Parkit/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     'parkapp',
     'crispy_forms',
-    'bootstrap4'
+
   
 ]
 
--- Parkit/urls.py
+++ Parkit/urls.py
@@ -14,20 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include ,re_path 
+from django.urls import path,include 
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('',include('parkapp.urls')),
 
-    re_path(r'^accounts/', include('registration.backends.simple.urls')),
-    re_path(r'^logout/', auth_views.logout_then_login), 
+ 
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
