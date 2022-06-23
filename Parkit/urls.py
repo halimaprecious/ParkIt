aed74cafe69d7240c675ce856c3c474764706b84
albright <sungutialbright@gmail.com>
2022-06-23 07:09:15 +0300

    ft:register token and stkpush urls

--- Parkit/urls.py
+++ Parkit/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """ 
+
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.urls import include, path
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'',include('parkapp.urls')),
+    path('api/v1/',include('parkapp.urls')),
+    path('admin/', admin.site.urls),
    
+   
+
 ]
+
